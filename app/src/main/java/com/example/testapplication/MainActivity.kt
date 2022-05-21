a81e3bb43aa08c4ccce984dd1cc42806a8541a9e
JS Kim <splineage@naver.com>
2022-05-21 18:29:09 +0900

    job&deferred

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -13,9 +13,21 @@ import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    val feeds = listOf<String>(
+        "https://www.npr.org/rss/rss.php?id=1001",
+        "http://rss.cnn.com/rss/cnn_topstories.rss",
+        "http://feeds.foxnews.com/foxnews/politics?format=xml",
+        "http://error"
+    )
+
     @OptIn(DelicateCoroutinesApi::class)
     private val serviceCallDispatcher = newSingleThreadContext(name = "ServiceCall")
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private val multiServiceCallDispatcher = newFixedThreadPoolContext(2, "IO")
+
     private val factory = DocumentBuilderFactory.newInstance()
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +39,8 @@ class MainActivity : AppCompatActivity() {
 //            }
 //            Log.e("TEST",headline.toString())
 //        }
-        asyncLoadNews()
+//        asyncLoadNews()
+        updateAsyncLoadNews()
 
     }
     @OptIn(DelicateCoroutinesApi::class)
@@ -39,6 +52,52 @@ class MainActivity : AppCompatActivity() {
                 binding.textView.text = "Found ${headlines.size} news"
             }
         }
+    }
+
+    @SuppressLint("SetTextI18n")
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+    private fun updateAsyncLoadNews() = GlobalScope.launch {
+        val requests = mutableListOf<Deferred<List<String>>>()
+        feeds.mapTo(requests){
+            asyncFetchHeadlines(it, multiServiceCallDispatcher)
+        }
+        requests.forEach{
+            it.join()
+//            it.await() // error
+        }
+        val headlines = requests
+            .filter { !it.isCancelled }
+            .flatMap {
+                it.getCompleted()
+            }
+        val failed = requests
+            .filter { it.isCancelled }
+            .size
+        val obtained = requests.size - failed
+        GlobalScope.launch(Dispatchers.Main) {
+            binding.textView.text = "Found ${headlines.size} news in ${obtained} feeds"
+            if (failed > 0){
+                binding.textWarning.text = "Failed to fetch $failed feeds"
+            }
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun asyncFetchHeadlines(
+        feed: String,
+        dispatcher: CoroutineDispatcher
+    ) = GlobalScope.async(dispatcher) {
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse(feed)
+        val news = xml.getElementsByTagName("channel").item(0)
+        (0 until news.childNodes.length)
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map { it as Element }
+            .filter { "item" == it.tagName }
+            .map {
+                it.getElementsByTagName("title").item(0).textContent
+            }
     }
 
     private fun fetchRssHeadlines(): List<String>{
